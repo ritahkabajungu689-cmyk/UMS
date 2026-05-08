d20c0ad7d9e0b9a1690a0a1debebd9ab473e9fd4
ritahkabajungu <ritahkabajungu@users.noreply.github.com>
2026-05-08 10:42:47 +0300

    Bypass PEP 668 restriction on Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt --break-system-packages
 python3.12 manage.py collectstatic --noinput --clear
 echo "BUILD END"
